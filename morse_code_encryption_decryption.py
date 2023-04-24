c377968fc8fa31af5b67dc5a51e0eb043b040f3c
Alessandro Silvestri <103107872+Alessandro-Silvestri@users.noreply.github.com>
2023-04-24 14:13:00 +0100

    Create morse_code_encryption_decryption.py

---
+++ morse_code_encryption_decryption.py
@@ -0,0 +1,54 @@
+'''
+MORSE CODE ENCRIPTION DECRIPTION
+
+Software for translating text to Morse code and vicevera.
+The program understand if you insert a text or a morse code and it checks if you insert a wrong morse code.
+Enjoy! :)
+Made by Alessandro Silvestri 2023 <alessandro.silvestri.work@gmail.com>
+'''
+
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.',
+'F':'..-.', 'G':'--.', 'H':'....', 'I':'..', 'J':'.---',
+'K':'-.-', 'L':'.-..', 'M':'--', 'N':'-.', 'O':'---',
+'P':'.--.', 'Q':'--.-', 'R':'.-.', 'S':'...', 'T':'-',
+'U':'..-', 'V':'...-', 'W':'.--', 'X':'-..-', 'Y':'-.--',
+'Z':'--..', '1':'.----', '2':'..---', '3':'...--', '4':'....-',
+'5':'.....', '6':'-....', '7':'--...', '8':'---..', '9':'----.',
+'0':'-----', ',':'--..--', '.':'.-.-.-', '?':'..--..', '/':'-..-.',
+'-':'-....-', '(':'-.--.', ')':'-.--.-', ' ':''}
+morse_values = list(MORSE_CODE_DICT.values())
+morse_keys = list(MORSE_CODE_DICT.keys())
+
+def text_to_morse(text: str):
+    ''' Converting test to morse'''
+    for i in text.upper():
+        print(MORSE_CODE_DICT[i])
+
+def morse_to_text(morse_input: str):
+    ''' Converting morse to and check a valid input'''
+    text = ''
+    morse_input = morse_input.split(' ')
+    for i in morse_input:
+        if i not in morse_values: # checking a valid input
+            text = 'Wrong input'
+            break
+        location = morse_values.index(i)
+        text += morse_keys[location]
+    print(text)
+
+def check_input(txt_input: str):
+    is_morse = False
+    for i in txt_input:
+        if i == '-' or i == '.' or i == ' ':
+            is_morse = True
+        else:
+            is_morse = False
+            break
+    return is_morse
+
+quest = input('insert text or morse (if it is morse use the spaces between the morse values): ')
+if check_input(quest):
+    morse_to_text(quest)
+else:
+    text_to_morse(quest)
+
