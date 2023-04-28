a0ce7fd7d24dfac2067827cdb28ee73a5a76da7f
apl_1978 <apl_1978@yahoo.com>
2023-04-28 17:37:51 +0300

    Optimized the check_input function. Added __main__. Formatted the code according to PEP8.

--- morse_code_encryption_decryption.py
+++ morse_code_encryption_decryption.py
@@ -7,51 +7,48 @@ Enjoy! :)
 Made by Alessandro Silvestri 2023 <alessandro.silvestri.work@gmail.com>
 '''
 
-MORSE_CODE_DICT = { 'A':'.-', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.',
-'F':'..-.', 'G':'--.', 'H':'....', 'I':'..', 'J':'.---',
-'K':'-.-', 'L':'.-..', 'M':'--', 'N':'-.', 'O':'---',
-'P':'.--.', 'Q':'--.-', 'R':'.-.', 'S':'...', 'T':'-',
-'U':'..-', 'V':'...-', 'W':'.--', 'X':'-..-', 'Y':'-.--',
-'Z':'--..', '1':'.----', '2':'..---', '3':'...--', '4':'....-',
-'5':'.....', '6':'-....', '7':'--...', '8':'---..', '9':'----.',
-'0':'-----', '!':'-.-.--', ',':'--..--', '.':'.-.-.-', '?':'..--..', '/':'-..-.',
-'-':'-....-', '(':'-.--.', ')':'-.--.-', ' ':'', '\'':'.----.', ':':'---...', '  ':' '}
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+                   'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+                   'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+                   'Z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+                   '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', '!': '-.-.--', ',': '--..--', '.': '.-.-.-', '?': '..--..', '/': '-..-.',
+                   '-': '-....-', '(': '-.--.', ')': '-.--.-', ' ': '', '\'': '.----.', ':': '---...', '  ': ' '}
 morse_values = list(MORSE_CODE_DICT.values())
 morse_keys = list(MORSE_CODE_DICT.keys())
+
 
 def text_to_morse(text: str):
     ''' Converting test to morse'''
     for i in text.upper():
         print(MORSE_CODE_DICT[i])
 
+
 def morse_to_text(morse_input: str):
     ''' Converting morse to text and checking the valid input'''
     text = ''
     morse_input = morse_input.split(' ')
     for i in morse_input:
-        if i not in morse_values: # checking a valid input
+        if i not in morse_values:  # checking a valid input
             text = 'Wrong input'
             break
         location = morse_values.index(i)
         text += morse_keys[location]
     print(text)
 
-def check_input(txt_input: str):
-    is_morse = False
-    for i in txt_input:
-        if i == '-' or i == '.' or i == ' ':
-            is_morse = True
-        else:
-            is_morse = False
-            break
-    return is_morse
 
-quest = input('''
-insert text or morse (if it is morse use the spaces between the morse values):
-insert double for separating the words:
--------> ''')
-if check_input(quest):
-    morse_to_text(quest)
-else:
-    text_to_morse(quest)
+def check_input(txt_input: str) -> bool:
+    return set(txt_input) | {'.', '-', ' '} == {'.', '-', ' '}
 
+
+if __name__ == '__main__':
+    quest = input('''
+    insert text or morse (if it is morse use the spaces between the morse values):
+    insert double for separating the words:
+    -------> ''')
+    if check_input(quest):
+        morse_to_text(quest)
+    else:
+        text_to_morse(quest)
